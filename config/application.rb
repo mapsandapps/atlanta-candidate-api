830689087f9470c008d4c57964bd941365c9b530
Jordan Streiff <jordan.streiff@toolbox9.com>
2017-06-13 10:32:59 -0400

    Update cors regex

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module AtlElection2017
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins /https:\/\/codeforatlanta\.github\.io\/atlanta\-candidate\-ui\//
+        origins /https:\/\/codeforatlanta\.github\.io/
         #origins /https:\/\/codeforatlanta\.github\.com/
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
