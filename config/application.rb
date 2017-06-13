a68b861ba11e6046643345c2f76cf2ce93e7456e
Th3R3p0 <th3r3p0@gmail.com>
2017-06-12 20:23:52 -0400

    cors regex will match better

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,8 @@ module AtlElection2017
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://codeforatlanta.github.io/atlanta-candidate-ui/'
+        origins /https:\/\/codeforatlanta\.github\.io\/atlanta\-candidate\-ui\//
+        #origins /https:\/\/codeforatlanta\.github\.com/
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
