b3008137cd6ae8cdf2612c2ed97e43e6a487b328
Th3R3p0 <th3r3p0@gmail.com>
2017-06-12 20:02:50 -0400

    fixed cors issue - https://github.com/jordanstreiff/atlanta-candidate-api/issues/18

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,13 @@ module AtlElection2017
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://codeforatlanta.github.io/atlanta-candidate-ui/'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
