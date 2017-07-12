3d7a9a43c43e775ee67162ff63ddb22ab0a9cd6f
Jordan Streiff <jordan.streiff@toolbox9.com>
2017-07-11 21:20:49 -0400

    Remove index endpoint for candidates (no longer in use)

--- app/controllers/api/candidates_controller.rb
+++ app/controllers/api/candidates_controller.rb
@@ -1,10 +1,6 @@
 class Api::CandidatesController < Api::ApiController
   helper AddressHelper
 
-  def index
-
-  end
-
   def show
     @candidate = Candidate.find(params[:id])
     render :show
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   namespace :api, defaults: { format: :json } do
-    resources :candidates, only: [:index, :show]
+    resources :candidates, only: [:show]
     resources :districts, only: [:index]
     resources :offices, only: [:index, :show]
   end
